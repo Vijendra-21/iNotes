ef0bb85a9a4f79557cf09243dfd9807b7654c64d
Vijendra-21 <vijendrakumawat8710@gmail.com>
2021-11-22 12:35:24 +0530

    Vdo 49, created hashing and web token

--- INotes-BackEnd/routes/auth.js
+++ INotes-BackEnd/routes/auth.js
@@ -1,29 +1,57 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Vijendra$kumawat21';
+
 // ...rest of the initial code omitted for simplicity.
 const { body, validationResult } = require('express-validator');
 
-//Create a user using : Post "/api/auth". It Doesn't require Auth
-router.post('/',[
+//Create a user using : Post "/api/auth/createuser". It Doesn't require Auth
+router.post('/createuser',[
     body('name','Enter a valid name').isLength({ min: 2 }),
       // email must be an email
   body('email','Enter a valid email').isEmail(),
   // password must be at least 5 chars long
   body('password','Password must be atleast 5 characters').isLength({ min: 5 }),
-], (req,res)=>{
-     // Finds the validation errors in this request and wraps them in an object with handy functions
+], async(req,res)=>{
+     // If there are errors,return bad request and the error
      const errors = validationResult(req);
      if (!errors.isEmpty()) {
        return res.status(400).json({ errors: errors.array() });
      }
-     User.create({
+     // Check whether the user with this email exists already
+     try{
+
+     let user = await User.findOne({email:req.body.email});
+     if(user){
+       return res.status(400).json({error: "Sorry a user with this email already exists"})
+     }
+
+     const salt = await bcrypt.genSalt(10);
+     const secPass = await bcrypt.hash(req.body.password,salt);
+
+     //Create a new user
+     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=> { console.log(err)
-        res.json({error:'Please enter a unique email',message: err.message})})
-    // res.send(req.body)
+        password: secPass,
+      });
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data,JWT_SECRET);
+
+      res.json({authtoken})
+    
+    }
+    catch(error){
+      console.error(error.message);
+      res.status(500).send("Some Error Occured")
+    }
 })
 module.exports = router
